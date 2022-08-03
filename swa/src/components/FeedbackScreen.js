395b5bac3ba860e2e3a1d7827ee3b84e1357b9d8
brucemoems <t-brucemoe@microsoft.com>
2022-08-02 19:03:24 -0700

    Add google

--- swa/src/components/FeedbackScreen.js
+++ swa/src/components/FeedbackScreen.js
@@ -10,7 +10,7 @@ import {
   Rating,
   RatingSize,
 } from "@fluentui/react";
-// import axios from "axios";
+import axios from "axios";
 
 function FeedbackScreen({ setPage }) {
   const [like, setLike] = useState("");
@@ -19,8 +19,30 @@ function FeedbackScreen({ setPage }) {
   const [dev, setDev] = useState("");
   const [rating, setRating] = useState(0);
   const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
   const handleSubmit = async (e) => {
-    setPage(3);
+    var data = JSON.stringify({
+      values: [[like, dislike, improve, dev, rating]],
+    });
+
+    var config = {
+      method: "post",
+      url:
+        "/api/google/spreadsheets/1O3Mqpx99uExd_LpRpdN4J-HZVI_5gjWQ-vFwJKUyuYw/values/sheet1:append?valueInputOption=USER_ENTERED",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: data,
+    };
+
+    axios(config)
+      .then(function(response) {
+        console.log(JSON.stringify(response.data));
+        setSuccess(true);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
   };
 
   return (
@@ -29,105 +51,111 @@ function FeedbackScreen({ setPage }) {
         width: "85%",
       }}
     >
-      <Text style={{ fontSize: FontSizes.medium }}>
-        {" "}
-        Please share your feedback about your intern experience. Your responses
-        are completely anonymous and will be used to improve the experience for
-        future interns.
-      </Text>
-      <div className="mt-4">
-        <TextField
-          placeholder="Engaging project, fun intern events, etc."
-          label="What did you like about your internship?"
-          className="mb-5"
-          multiline
-          autoAdjustHeight
-          onChange={(_, newValue) => {
-            setLike(newValue);
-          }}
-        />
-        <TextField
-          placeholder="Too much work, not enough socials, etc."
-          label="What did you dislike about your internship?"
-          className="mb-5"
-          multiline
-          autoAdjustHeight
-          onChange={(_, newValue) => {
-            setDislike(newValue);
-          }}
-        />
-        <TextField
-          placeholder="More networking events, clearer project expectations, etc."
-          label="What could have been improved?"
-          className="mb-5"
-          multiline
-          autoAdjustHeight
-          onChange={(_, newValue) => {
-            setImprove(newValue);
-          }}
-        />
-        <TextField
-          placeholder="Yes, because..."
-          label="Would you return fulltime to DevDiv?"
-          className="mb-5"
-          multiline
-          autoAdjustHeight
-          onChange={(_, newValue) => {
-            setDev(newValue);
-          }}
-        />
-        Rate your internship experience
-        <Rating
-          max={10}
-          size={RatingSize.Large}
-          defaultRating={1}
-          className="mb-5"
-          onChange={(_, newValue) => {
-            setRating(newValue);
-          }}
-        />
-        {!loading ? (
-          <DefaultButton
+      {!success ? (
+        <div>
+          <Text style={{ fontSize: FontSizes.medium }}>
+            {" "}
+            Please share your feedback about your intern experience. Your
+            responses are completely anonymous and will be used to improve the
+            experience for future interns.
+          </Text>
+          <TextField
+            placeholder="Engaging project, fun intern events, etc."
+            label="What did you like about your internship?"
+            className="mb-5 mt-4"
+            multiline
+            autoAdjustHeight
+            onChange={(_, newValue) => {
+              setLike(newValue);
+            }}
+          />
+          <TextField
+            placeholder="Too much work, not enough socials, etc."
+            label="What did you dislike about your internship?"
             className="mb-5"
-            onClick={(e) => handleSubmit(e)}
-            variant="primary"
-          >
-            Submit
-          </DefaultButton>
-        ) : (
-          <Spinner size={SpinnerSize.large} className="mb-5"></Spinner>
-        )}
-        <div className="mb-5" style={{ display: "flex", flexDirection: "row" }}>
+            multiline
+            autoAdjustHeight
+            onChange={(_, newValue) => {
+              setDislike(newValue);
+            }}
+          />
+          <TextField
+            placeholder="More networking events, clearer project expectations, etc."
+            label="What could have been improved?"
+            className="mb-5"
+            multiline
+            autoAdjustHeight
+            onChange={(_, newValue) => {
+              setImprove(newValue);
+            }}
+          />
+          <TextField
+            placeholder="Yes, because..."
+            label="Would you return fulltime to DevDiv?"
+            className="mb-5"
+            multiline
+            autoAdjustHeight
+            onChange={(_, newValue) => {
+              setDev(newValue);
+            }}
+          />
+          Rate your internship experience
+          <Rating
+            max={10}
+            size={RatingSize.Large}
+            defaultRating={1}
+            className="mb-5"
+            onChange={(_, newValue) => {
+              setRating(newValue);
+            }}
+          />
+          {!loading ? (
+            <DefaultButton
+              className="mb-5"
+              onClick={(e) => handleSubmit(e)}
+              variant="primary"
+            >
+              Submit
+            </DefaultButton>
+          ) : (
+            <Spinner size={SpinnerSize.large} className="mb-5"></Spinner>
+          )}
+        </div>
+      ) : (
+        <div className="mb-3">
+          <Text> Thanks! Your feedback has been submitted. </Text>
+        </div>
+      )}
+      <div className="mb-5" style={{ display: "flex", flexDirection: "row" }}>
+        <ActionButton
+          onClick={(e) => {
+            console.log(setLoading);
+            console.log(like);
+            console.log(dislike);
+            console.log(improve);
+            console.log(dev);
+            console.log(rating);
+            setPage(1);
+          }}
+          iconProps={{ iconName: "Back" }}
+        >
+          Back
+        </ActionButton>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            width: "100%",
+          }}
+        >
           <ActionButton
             onClick={(e) => {
-              console.log(setLoading);
-              console.log(like);
-              console.log(dislike);
-              console.log(improve);
-              console.log(dev);
-              console.log(rating);
-              setPage(1);
+              setPage(3);
             }}
-            iconProps={{ iconName: "Back" }}
+            iconProps={{ iconName: "Forward" }}
           >
-            Back
+            Next
           </ActionButton>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "flex-end",
-              width: "100%",
-            }}
-          >
-            <ActionButton
-              onClick={(e) => {
-                setPage(3);
-              }}
-              iconProps={{ iconName: "Forward" }}
-            >
-              Next
-            </ActionButton>
-          </div>
         </div>
       </div>
     </div>
--- swa/src/components/FormScreen.js
+++ swa/src/components/FormScreen.js
@@ -15,6 +15,7 @@ function FormScreen({ setPage }) {
   const [last, setLast] = useState("");
   const [email, setEmail] = useState("");
   const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
   const handleSubmit = async (e) => {
     try {
       setLoading(true);
@@ -32,8 +33,8 @@ function FormScreen({ setPage }) {
         })
         .then((response) => {
           console.log(response);
+          setSuccess(true);
           setLoading(false);
-          setPage(3);
         });
     } catch (err) {
       console.error(err);
@@ -46,67 +47,75 @@ function FormScreen({ setPage }) {
         width: "85%",
       }}
     >
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        <Text className="mb-3" style={{ fontSize: FontSizes.medium }}>
-          {" "}
-          Fill out this form if you would like to be added to the DevDiv former
-          intern mailing list!{" "}
-        </Text>
-        <Text style={{ fontSize: FontSizes.medium }}>
-          {" "}
-          Note: Your contact info and feedback will be used for Microsoft
-          internal purposes only{" "}
-        </Text>
-      </div>
-      <Text style={{ fontSize: FontSizes.medium }}> </Text>
-      <div className="mt-4">
-        <TextField
-          placeholder="John"
-          label="First Name"
-          required
-          onChange={(_, newValue) => setFirst(newValue)}
-          className="mb-5"
-        />
-        <TextField
-          placeholder="Doe"
-          label="Last Name"
-          required
-          className="mb-5"
-          onChange={(_, newValue) => {
-            setLast(newValue);
-          }}
-        />
-        <TextField
-          placeholder="johndoe@outlook.com"
-          label="Email"
-          required
-          className="mb-5"
-          onChange={(_, newValue) => {
-            setEmail(newValue);
-          }}
-        />
+      {!success ? (
+        <div>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <Text className="mb-3" style={{ fontSize: FontSizes.medium }}>
+              {" "}
+              Fill out this form if you would like to be added to the DevDiv
+              former intern mailing list!{" "}
+            </Text>
+            <Text style={{ fontSize: FontSizes.medium }}>
+              {" "}
+              Note: Your contact info and feedback will be used for Microsoft
+              internal purposes only{" "}
+            </Text>
+          </div>
+          <Text style={{ fontSize: FontSizes.medium }}> </Text>
+          <div className="mt-4">
+            <TextField
+              placeholder="John"
+              label="First Name"
+              required
+              onChange={(_, newValue) => setFirst(newValue)}
+              className="mb-5"
+            />
+            <TextField
+              placeholder="Doe"
+              label="Last Name"
+              required
+              className="mb-5"
+              onChange={(_, newValue) => {
+                setLast(newValue);
+              }}
+            />
+            <TextField
+              placeholder="johndoe@outlook.com"
+              label="Email"
+              required
+              className="mb-5"
+              onChange={(_, newValue) => {
+                setEmail(newValue);
+              }}
+            />
 
-        {!loading ? (
-          <DefaultButton
-            className="mb-5"
-            onClick={(e) => handleSubmit(e)}
-            variant="primary"
-          >
-            Submit
-          </DefaultButton>
-        ) : (
-          <Spinner size={SpinnerSize.large} className="mb-5"></Spinner>
-        )}
-        <div className="mb-5">
-          <ActionButton
-            onClick={(e) => {
-              setPage(2);
-            }}
-            iconProps={{ iconName: "Back" }}
-          >
-            Back
-          </ActionButton>
+            {!loading ? (
+              <DefaultButton
+                className="mb-5"
+                onClick={(e) => handleSubmit(e)}
+                variant="primary"
+              >
+                Submit
+              </DefaultButton>
+            ) : (
+              <Spinner size={SpinnerSize.large} className="mb-5"></Spinner>
+            )}
+          </div>
         </div>
+      ) : (
+        <div className="mb-3">
+          <Text> Successfully joined mailing list! </Text>
+        </div>
+      )}
+      <div className="mb-5">
+        <ActionButton
+          onClick={(e) => {
+            setPage(2);
+          }}
+          iconProps={{ iconName: "Back" }}
+        >
+          Back
+        </ActionButton>
       </div>
     </div>
   );
